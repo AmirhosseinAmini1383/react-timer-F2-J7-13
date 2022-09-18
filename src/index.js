ddc4d51ce3f9935c7eaf3e4295a976e942db1f31
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2022-09-18 17:31:37 +0430

    #8 component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>
+          Time it is {new Date().toLocaleTimeString()} 
+      </h2>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1>
+        پر قدرت ادامه میدم :) قراره یه فرانت کار خفن بشم :) قول میدم به خودم
+      </h1>
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Hello/>
+        <Timer/>
+      </div>
+    )
+  }
+}
+// const elem=new App;
+
+const Tick=()=>{
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    // elem.render()
+    <App/>
+    );
+}
+
+setInterval(()=>{
+  Tick();
+},1000)
